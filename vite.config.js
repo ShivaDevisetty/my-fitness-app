2216672f213d2e63c1e24ccd0bd45ea17742b4d1
Shiva Devisetty <84583106+ShivaDevisetty@users.noreply.github.com>
2026-06-23 15:43:04 -0500

    Change base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/myplan/', // GitHub Pages repo name — change if your repo name is different
+  base: '/my-fitness-app/', // GitHub Pages repo name — change if your repo name is different
   build: {
     outDir: 'dist'
   }
